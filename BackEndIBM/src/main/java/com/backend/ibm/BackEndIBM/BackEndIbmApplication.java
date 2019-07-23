90331b6ac59b834598601ec816441931db402b69
Jorge Alejandro <alejoved@gmail.com>
2019-07-22 21:21:55 -0500

    init

---
+++ BackEndIBM/src/main/java/com/backend/ibm/BackEndIBM/BackEndIbmApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ibm.BackEndIBM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndIbmApplication.class, args);
+	}
+
+}
